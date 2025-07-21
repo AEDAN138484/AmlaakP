61127ab7d8a37078bc12d1f0d965cc4eedeaf814
AEDAN138484 <154571593+AEDAN138484@users.noreply.github.com>
2025-07-21 03:54:56 -0800

    second

--- server.js
+++ server.js
@@ -4,7 +4,4 @@ const path = require('path');
 const express = require('express');
 
 const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`سرور در http://localhost:${port} اجرا شد`);
-});
+app.listen(port, () => console.log(`Listening on ${port}`));
